030bf0f2a55a2cce410221554ec45322c3f2fbf2
JSLY <2029872426@qq.com>
2026-03-28 00:43:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
-from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
-
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+from astrbot.api.star import Context, Star
+from astrbot.api import logger # 使用 astrbot 提供的 logger 接口
+from astrbot.api import AstrBotConfig
+import requests
 class MyPlugin(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: AstrBotConfig): # AstrBotConfig 继承自 Dict，拥有字典的所有方法
         super().__init__(context)
+        self.config = config
+        print(self.config)
 
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
-
+    @filter.event_message_type(filter.EventMessageType.PRIVATE_MESSAGE)
+    @filter.command("use")
+    async def use(self, event: AstrMessageEvent, url:str, token:str, userId:str|int, key:str):
+        '''这是一个兑换码兑换指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。非常建议填写。
+        body=f"""{{
+            "key": "{key}"
+        }}"""
+        response = requests.request("POST", url, data = body, headers = {
+            "Content-Type": "application/json", 
+            "Authorization": f"Bearer {token}",
+            "New-Api-User": f"{userId}"
+        })
+        
     async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        '''可选择实现 terminate 函数，当插件被卸载/停用时会调用。'''
